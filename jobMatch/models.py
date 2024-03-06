b1c2799550fe02d1d2748ec7d444643255d88f57
Az El Arab Chakour <56452019+azelarabchakour@users.noreply.github.com>
2024-03-06 16:24:31 +0100

    adding models

---
+++ jobMatch/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    firstName = models.CharField(max_length=200, null=True)
+    lastName = models.CharField(max_length=200, null=True)
+    email =models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class JobDescription(models.Model):
+    title = models.CharField(max_length=200, null=True)
+
+class Cv(models.Model):
+    
