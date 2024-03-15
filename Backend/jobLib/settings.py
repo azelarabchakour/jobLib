3a7602dd6725c23d95beada35a4b505a1577c352
Az El Arab Chakour <56452019+azelarabchakour@users.noreply.github.com>
2024-03-15 23:13:11 +0000

    settings

--- Backend/jobLib/settings.py
+++ Backend/jobLib/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'employee',
+    'employer',
+    'authentication',
+    'jobMatch',
+
 ]
 
 MIDDLEWARE = [
@@ -73,12 +79,13 @@ WSGI_APPLICATION = 'jobLib.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 
 # Password validation
