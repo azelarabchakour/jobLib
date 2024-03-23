8ac9877fca07830e8c263083576ab9ca07f6f308
Hajar Essaoudi <109491959+HajarEssaoudi@users.noreply.github.com>
2024-03-23 21:42:41 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
-// import { Navbar } from 'react-bootstrap';
 import './App.css';
-// import Signup from './authentication/SignUp/signup';
 import Login from './authentication/Login/login';
 import Signup from './authentication/SignUp/signup';
-import Navbar from './hompage/navBar';
 
 
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      {/* <Signup/>
-      <Login/> */}
+      <Signup/>
+      <Login/> 
     </>
   );
 }
