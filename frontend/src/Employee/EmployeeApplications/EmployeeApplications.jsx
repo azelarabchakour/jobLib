c22eee6a1fb279c616a6c2c649b504b0b142d9c3
Hajar Essaoudi <109491959+HajarEssaoudi@users.noreply.github.com>
2024-04-08 22:47:04 +0000

    last update

--- frontend/src/Employee/EmployeeApplications/EmployeeApplications.jsx
+++ frontend/src/Employee/EmployeeApplications/EmployeeApplications.jsx
@@ -32,30 +32,32 @@ function ApplicationPage() {
 
   return (
     <>
-      <EmployeeNavbar></EmployeeNavbar>
+      <EmployeeNavbar />
       <div className="card-old-job-descriptions">
-      <h1>Jobs Applied</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : (
-        <ul>
-          {jobs.map(job => (
-            <li key={job.id} className="job-card">
-              <div className="job-card-content">
-                <h2 className="job-title">Job ID: {job.id}</h2>
-                <p className="job-description">Application Date: {job.application_date}</p>
-                <p className="job-description">Application Status: {job.applicationStatus}</p>
-              </div>
-              <div className="job-actions">
-                {/* Add your buttons here */}
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+        <h1>Jobs Applied</h1>
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          <ul>
+            {jobs.map(job => (
+              <li key={job.id} className="job-card">
+                <div className="job-card-content">
+                  <h2 className="job-title">{job.job_posting.jobTitle}</h2>
+                  <p className="job-description">{job.job_posting.jobDescription}</p>
+                  <p className="job-description"><b>Application Date:</b> {job.application_date}</p>
+                  <p className="job-description"><b>Application Status:</b> {job.applicationStatus}</p>
+                  <p className="job-description"><b>Company/Hr:</b> {job.job_posting.employer.user.username}</p>
+                </div>
+                <div className="job-actions">
+                  {/* Add your buttons here */}
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </>
   );
 }
