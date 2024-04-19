554be044b97dce81a27a54439483781f791f6f86
Az El Arab Chakour <56452019+azelarabchakour@users.noreply.github.com>
2024-04-19 20:39:27 +0100

    job posting

--- frontend-v2/src/Components/JobPostingCard.js
+++ frontend-v2/src/Components/JobPostingCard.js
@@ -1,41 +1,24 @@
 import {
-    Card,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-  } from "@material-tailwind/react";
-   
-  export default function JobPostingCard() {
-    return (
-      <Card className="mt-6 w-4/5 max-h-48 overflow-hidden">
-        <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            UI/UX Review Check
-          </Typography >
-          <Typography className="truncate">
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-            
-            
-            
-            
-          </Typography>
-        </CardBody>
-        <CardFooter className="pt-0">
-          <Button>Read More</Button>
-        </CardFooter>
-      </Card>
-    );
-  }
-  
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+
+export default function JobPostingCard(props) {
+  return (
+    <Card className="mt-6 w-4/5 max-h-48 overflow-hidden">
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          {props.jobTitle}
+        </Typography>
+        <Typography className="truncate">{props.jobDescription}</Typography>
+      </CardBody>
+      <CardFooter className="flex items-center justify-between">
+        <Button>Read More</Button>
+        <Typography className="font-normal">{props.salary}</Typography>
+      </CardFooter>
+    </Card>
+  );
+}
--- frontend-v2/src/Employer/Jobs.jsx
+++ frontend-v2/src/Employer/Jobs.jsx
@@ -1,20 +1,123 @@
 import { Button } from "@material-tailwind/react";
-import { useNavigate } from 'react-router-dom';
-import React from 'react';
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import JobPostingCard from "../Components/JobPostingCard";
 
 export default function Jobs() {
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        // Clear any stored authentication token or user data
-        localStorage.removeItem('accessToken'); // the token is stored in localStorage
-        localStorage.removeItem('refreshToken');
-        navigate('/'); // Navigate to login page
-    };
+  const [oldJobDescriptions, setOldJobDescriptions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    // Clear any stored authentication token or user data
+    localStorage.removeItem("accessToken"); // the token is stored in localStorage
+    localStorage.removeItem("refreshToken");
+    navigate("/"); // Navigate to login page
+  };
+
+  useEffect(() => {
+    const authToken = localStorage.getItem("accessToken");
+    if (!authToken) {
+      setError("You need to login to access this page.");
+      navigate("/login");
+      setLoading(false);
+    } else {
+      axios
+        .get("http://127.0.0.1:8000/employer/jobs/", {
+          headers: {
+            Authorization: `JWT ${authToken}`,
+          },
+        })
+        .then((response) => {
+          setOldJobDescriptions(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching old job descriptions:", error);
+          setError(
+            "Error fetching old job descriptions. Please try again later."
+          );
+          setLoading(false);
+        });
+    }
+  }, []);
+
+  const handleEdit = (jobId) => {
+    // Redirect to the edit job page, passing the jobId as a parameter
+    navigate(`/employer/edit-job/${jobId}/`);
+  };
+
+  const handleDelete = (jobId) => {
+    // Send a DELETE request to the server to delete the job
+    const authToken = localStorage.getItem("accessToken");
+    axios
+      .delete(`http://127.0.0.1:8000/employer/jobs/${jobId}/deleteJob/`, {
+        headers: {
+          Authorization: `JWT ${authToken}`,
+        },
+      })
+      .then((response) => {
+        // Remove the deleted job from the state
+        setOldJobDescriptions(
+          oldJobDescriptions.filter((job) => job.id !== jobId)
+        );
+      })
+      .catch((error) => {
+        console.error("Error deleting job:", error);
+        setError("Error deleting job. Please try again later.");
+      });
+  };
+
+  const handleJobCardClick = (jobId) => {
+    navigate(`/employer/${jobId}/applications`);
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
     return (
-        <div>
-        <Button onClick={handleLogout}>logout</Button>
-          Hello world Employer
-        </div>
+      <div className="employer-error-message">
+        <p>{error}</p>
+      </div>
     );
   }
+
+  return (
+    <div>
+      <Button onClick={handleLogout}>logout</Button>
+      Hello world Employer
+      {oldJobDescriptions.map((job) => (
+        // <li key={job.id} className="job-card">
+        //   <div
+        //     className="job-application-card-content"
+        //     onClick={() => handleJobCardClick(job.id)}
+        //   >
+        //     <h3 className="job-title">{job.jobTitle}</h3>
+        //     <p className="job-description">{job.jobDescription}</p>
+        //     <p className="job-salary">
+        //       Salary: ${job.salaryMin} - ${job.salaryMax}
+        //     </p>
+        //   </div>
+        //   <div className="job-actions">
+        //     <button className="Edit" onClick={() => handleEdit(job.id)}>
+        //       Edit
+        //     </button>
+        //     <button className="delete" onClick={() => handleDelete(job.id)}>
+        //       Delete
+        //     </button>
+        //   </div>
+        // </li>
+        <JobPostingCard
+          jobTitle={job.jobTitle}
+          jobDescription={job.jobDescription}
+          salary = {job.salaryMin + "$ - " + job.salaryMax + "$"} 
+        ></JobPostingCard>
+      ))}
+    </div>
+  );
+}
