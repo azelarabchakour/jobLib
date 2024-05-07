17664cd692e4a3e9c73ca6141cbf3a4901ad6d82
Hajar Essaoudi <109491959+HajarEssaoudi@users.noreply.github.com>
2024-05-07 15:15:48 +0100

    just pdf

--- frontend-v2/src/Employee/UploadButton.jsx
+++ frontend-v2/src/Employee/UploadButton.jsx
@@ -70,6 +70,20 @@ function UploadButton() {
             onDrop={handleDrop}
             onDragOver={handleDragOver}
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-5 w-5 left-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+              />
+            </svg>
             <p className="text-myBlue-300 mt-24 text-center">
               choose a file or drag it here
             </p>
@@ -78,27 +92,13 @@ function UploadButton() {
                 id="resume-upload"
                 type="file"
                 className="hidden"
-                accept=".pdf,.doc,.docx"
+                accept=".pdf"
                 onChange={handleFileChange}
               />
               <label
                 htmlFor="resume-upload"
                 className="cursor-pointer text-myBlue-950 px-4 py-2 rounded-md flex items-center gap-3"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={2}
-                  stroke="currentColor"
-                  className="h-5 w-5 left-3"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                  />
-                </svg>
                 Upload Your Resume
               </label>
             </div>
