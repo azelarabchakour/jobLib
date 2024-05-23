19d1262e9ca04494947c375c7a7504feebdc7aa7
Hajar Essaoudi <109491959+HajarEssaoudi@users.noreply.github.com>
2024-05-23 22:32:03 +0100

    join as Recruiter

--- frontend-v2/src/Authentication/AuthEmployer.jsx
+++ frontend-v2/src/Authentication/AuthEmployer.jsx
@@ -149,29 +149,29 @@ export default function AuthEmployee() {
       </Button> */}
 
       <a href="#" className="inline-block">
-          <Button
-            size="sm"
-            variant="text"
-            className="flex items-center gap-2 hover:bg-mantis-700 hover:text-myBlue-50"
-            onClick={handleOpen}
+        <Button
+          size="sm"
+          variant="text"
+          className="flex items-center gap-2 hover:bg-mantis-700 hover:text-myBlue-50"
+          onClick={handleOpen}
+        >
+          Join as a Recruiter
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-4 w-4"
           >
-            Join as a Client
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-4 w-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-              />
-            </svg>
-          </Button>
-        </a>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+            />
+          </svg>
+        </Button>
+      </a>
 
       <Dialog
         size="sm"
